248a25e8c268bfda94bdddb2f99c077d012c7b64
abdulhamid07 <abdulhamid.dev@gmail.com>
2022-11-23 10:56:29 +0700

    react/no-unescaped-entities

--- pages/detail.js
+++ pages/detail.js
@@ -37,26 +37,26 @@ function detail() {
         <div className="md:w-8/12 w-full mx-auto leading-relaxed text-justify">
           <p className="text-xl mb-4">
             Male sixth sea it a. Brought was signs female darkness signs form
-            cattle land grass whose from subdue also they're their brought seas
-            isn't, to day from bearing grass third midst after beginning man
-            which you're. Dry, gathering beginning given made them evening.
+            cattle land grass whose from subdue also theyre their brought seas
+            isnt, to day from bearing grass third midst after beginning man
+            which youre. Dry, gathering beginning given made them evening.
           </p>
           <p className="mb-4">
             Lights dry. Thing, likeness, forth shall replenish upon abundantly
             our green. Seed green sea that lesser divided creature beginning
             land him signs stars give firmament gathered. Wherein there their
-            morning a he grass. Don't made moving for them bring creature us
+            morning a he grass. Dont made moving for them bring creature us
             you'll tree second deep good unto good may. Us yielding.
           </p>
           <p>
             Have. Man upon set multiply moved from under seasons abundantly
-            earth brought a. They're open moved years saw isn't morning
+            earth brought a. Theyre open moved years saw isnt morning
             darkness. Over, waters, every let wherein great were fifth saw was
-            lights very our place won't and him Third fourth moving him whales
-            behold. Beast second stars lights great was don't green give subdue
-            his. Third given made created, they're forth god replenish have
-            whales first can't light was. Herb you'll them beast kind divided.
-            Were beginning fly air multiply god Yielding sea don't were forth.
+            lights very our place wont and him Third fourth moving him whales
+            behold. Beast second stars lights great was dont green give subdue
+            his. Third given made created, theyre forth god replenish have
+            whales first cant light was. Herb youll them beast kind divided.
+            Were beginning fly air multiply god Yielding sea dont were forth.
           </p>
         </div>
       </Container>
